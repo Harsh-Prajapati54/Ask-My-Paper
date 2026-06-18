252a323d7598a08ad624c43f6a654ddd4b2e55ef
Harsh Prajapati <145095712+Harsh-Prajapati54@users.noreply.github.com>
2026-06-18 22:43:05 +0530

    commit

--- src/ingestion/loader.py
+++ src/ingestion/loader.py
@@ -1,2 +1,19 @@
-import pymupdf
-print(pymupdf.version())
+from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+
+class DocumentLoader:
+    
+    def load(self, file_path):
+        loader = PyMuPDF4LLMLoader(file_path)
+        docs = loader.load()
+        return docs
+
+
+# Actually call it
+if __name__ == "__main__":
+    file_path = r"C:\Ask My Paper\Data\LAB3_IOT _2026.pdf"
+    
+    dl = DocumentLoader()
+    docs = dl.load(file_path)
+    
+    print(f"Loaded {len(docs)} pages")
+    print(docs[0].page_content[:500])  # preview first 500 chars
