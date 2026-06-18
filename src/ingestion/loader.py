9e30d684ac349c99e264ff43e4f866561a9cd29c
Harsh Prajapati <145095712+Harsh-Prajapati54@users.noreply.github.com>
2026-06-18 23:36:50 +0530

    commit

--- src/ingestion/loader.py
+++ src/ingestion/loader.py
@@ -1,19 +1,26 @@
 from langchain_pymupdf4llm import PyMuPDF4LLMLoader
+import pprint
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 class DocumentLoader:
-    
     def load(self, file_path):
-        loader = PyMuPDF4LLMLoader(file_path)
+        loader = PyMuPDF4LLMLoader(file_path,
+                                   mode = "page",
+                                   extract_images=True,
+                                   image_parser = LLMImageBlobParser(api_key=api_key, model="gpt-4o-mini", max_image_size=1024)
+                                   )
         docs = loader.load()
         return docs
 
-
-# Actually call it
 if __name__ == "__main__":
-    file_path = r"C:\Ask My Paper\Data\LAB3_IOT _2026.pdf"
+    file_path = r"C:\Ask My Paper\Data\Copy of AI Engineering-25-125.pdf"
     
     dl = DocumentLoader()
     docs = dl.load(file_path)
     
     print(f"Loaded {len(docs)} pages")
-    print(docs[0].page_content[:500])  # preview first 500 chars
+    pprint.pp(docs[2].metadata)
